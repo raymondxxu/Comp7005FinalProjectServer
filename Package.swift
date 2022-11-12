5b75aa7572e34fc018861fe2a31bb2599660f676
Raymond <raymondxxu>
2022-11-12 14:55:21 -0800

    Remove local dependency

--- Package.swift
+++ Package.swift
@@ -6,15 +6,16 @@ import PackageDescription
 let package = Package(
     name: "Server",
     dependencies: [
-        .package(path: "../Socket"),
-        .package(path: "../CommonLib")
+        .package(url: "https://github.com/raymondxxu/Comp7005FinalProjectCommonLib", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "Server",
-            dependencies: ["Socket", "CommonLib"]),
+            dependencies: [
+                .product(name: "CommonLib", package: "Comp7005FinalProjectCommonLib")
+            ]),
         .testTarget(
             name: "ServerTests",
             dependencies: ["Server"]),
